6eba693e1002de10d6b133ec12af994d3ae14ab8
Johan Eliasson <jeliasson@users.noreply.github.com>
2022-03-22 19:15:43 +0100

    RedwoodJS 0.49.1

--- web/src/pages/HomePage/HomePage.test.tsx
+++ web/src/pages/HomePage/HomePage.test.tsx
@@ -1,6 +1,9 @@
-import { render } from '@redwoodjs/testing'
+import { render } from '@redwoodjs/testing/web'
 
 import HomePage from './HomePage'
+
+//   Improve this test with help from the Redwood Testing Doc:
+//   https://redwoodjs.com/docs/testing#testing-pages-layouts
 
 describe('HomePage', () => {
   it('renders successfully', () => {
